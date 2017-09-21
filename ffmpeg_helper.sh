c15ef017adb3b6d1c4da86363546fb6c79091ddd
StrawsonDesign <james@strawsondesign.com>
2017-09-21 16:45:55 -0700

    dual-pass option

--- ffmpeg_helper.sh
+++ ffmpeg_helper.sh
@@ -11,7 +11,7 @@ shopt -s globstar # for recursive for loops
 # start with map for video, add to it later
 using_libx264=false;
 force_external_sub=false;
-auto_subs=false;
+autosubs=false;
 map_all_eng=false;
 audio_metadata=""
 subtitle_metadata=""
@@ -19,12 +19,14 @@ indir=false
 outdir=false
 preview=false
 onefile=false
+twopass=false
+sub_metadata="-metadata:s:s:0 Title=\"English\" -metadata:s:s:0 language=eng"
 
 # other general options
 # -n tells ffmpeg to skip files if completed, not necessary anymore since we 
 # check in the script before executing, but doesn't hurt to keep
 # also silence the initial ffmpeg prints and turn stats back on
-other="-n -hide_banner -v fatal -stats"
+other="-hide_banner -v fatal -stats"
 
 
 #print helpful usage to screen
@@ -94,10 +96,15 @@ if ! [ $container == "srt" ]; then
 	# ask video codec question
 	echo " "
 	echo "Which Video codec to use?"
-	select opt in "x264_rf19_10M" "x264_rf18_10M" "x264_rf18_slow" "x264_rf20_slow" "x264_rf20_fast" "x264_18_fast" "nvenc_h264" "h265_8bit" "h265_8bit_fast" "h265_10bit" "copy"; do
+	select opt in "twopass_12M" "x264_rf19_10M" "x264_rf18_10M" "x264_rf18_slow" "x264_rf20_slow" "x264_rf20_fast" "x264_18_fast" "nvenc_h264" "h265_8bit" "h265_8bit_fast" "h265_10bit" "copy"; do
 		case $opt in
 		copy )
 			vopts="-c:v copy"
+			break;;
+		twopass_12M )
+			vopts="-c:v libx264 -preset slow -b:v 12288k"
+			using_libx264=true;
+			twopass=true;
 			break;;
 		x264_rf18_slow )
 			vopts="-c:v libx264 -preset slow -crf 18"
@@ -144,7 +151,7 @@ if ! [ $container == "srt" ]; then
 	if $using_libx264 ; then
 		echo " "
 		echo "which h264 level, 4.1 recommended for most bluray"
-		select opt in "4.1_1080" "3.1_dvd" "auto" "4.2_1080"; do
+		select opt in "auto" "4.1_1080" "3.1_dvd"  "4.2_1080"; do
 			case $opt in
 			auto )
 				break;;
@@ -197,8 +204,8 @@ if ! [ $container == "srt" ]; then
 	# ask audio tracks question
 	echo " "
 	echo "Which audio tracks to use?"
-	echo "note, mapping all english audio tracks also maps all english subtitles"
-	echo "and subtitle mode is forced to auto"
+	#echo "note, mapping all english audio tracks also maps all english subtitles"
+	#echo "and subtitle mode is forced to auto"
 	select opt in  "first" "second" "all_english" "all" "first+commentary" ; do
 		case $opt in
 		all_english)
@@ -226,17 +233,17 @@ if ! [ $container == "srt" ]; then
 	# ask audio codec question
 	echo " "
 	echo "Which audio codec to use?"
-	select opt in "aac_stereo_downmix" "aac_stereo" "aac_5.1" "copy"; do
+	select opt in "aac_5.1" "aac_stereo" "aac_stereo_downmix" "copy"; do
 		case $opt in
 		aac_stereo )
-			aopts="-c:a aac -b:a 160k"
+			aopts="-c:a aac -b:a 192k"
 			break;;
 		aac_stereo_downmix )
-			aopts="-c:a aac -b:a 160k"
+			aopts="-c:a aac -b:a 192k"
 			aopts="$aopts -af \"pan=stereo|FL < 1.0*FL + 0.707*FC + 0.707*BL|FR < 1.0*FR + 0.707*FC + 0.707*BR\""
 			break;;
 		aac_5.1 )
-			aopts="-c:a aac -b:a 480k"
+			aopts="-c:a aac -b:a 576k"
 			break;;
 		copy )
 			aopts="-c:a copy"
@@ -246,25 +253,24 @@ if ! [ $container == "srt" ]; then
 		esac
 	done
 
-	# ask subtitle question only if english audio maps haven't been set
-	# if it was set, english subtitles will be pulled automatically too
-	# along with an external srt if it exists
+	# ask subtitle question
 	echo " "
 	echo "What to do with subtitles?"
-	select opt in "use_external_srt" "keep_first" "keep_all" "none"; do
+	echo "Auto will select external srt if available"
+	echo "otherwise will grab first embedded sub"
+	select opt in "auto" "use_external_srt" "keep_first" "keep_all" "none"; do
 		case $opt in
+		auto)
+			autosubs=true;
+			break;;
 		keep_all )
 			smaps="-map 0:s"
 			break;;
 		keep_first )
 			smaps="-map 0:s:0"
-			# force english language metadata since it is sometimes "unknown"
-			sub_metadata="-metadata:s:s:0 Title=\"English\" -metadata:s:s:0 language=eng"
 			break;;
 		use_external_srt )
 			force_external_sub=true;
-			# force english language metadata since it is sometimes "unknown"
-			sub_metadata="-metadata:s:s:0 Title=\"English\" -metadata:s:s:0 language=eng"
 			smaps="-map 1:s"
 			break;;
 		none )
@@ -367,14 +373,27 @@ do
 	# if using external subtitles, add to inputs
 	if [ $force_external_sub == true ]; then
 		ins="$ins -i \"$fpath.srt\""
+
+	# if using autosubs, check if externals exist
+	else if [ $autosubs == true ]; then
+		if [ -f "$fpath.srt" ]; then
+			ins="$ins -i \"$fpath.srt\""
+			smaps="-map 1:s"
+		else
+			smaps="-map 0:s:0"
+		fi
 	fi
 
 	#combine options into ffmpeg string
 	maps="$vmaps $amaps $smaps"
 	metadata="-metadata title=\"$fname\" $video_metadata $audio_metadata $sub_metadata"
 	#metadata="$video_metadata $audio_metadata $sub_metadata"
-	command="ffmpeg $other $ins $maps $vopts $lopts $filters $aopts $sopts $metadata \"$outfull\""
-
+	if [ $twopass == true ]; then
+		command="ffmpeg -y $other $ins $maps $vopts -pass 1 $aopts -f $container /dev/null && ffmpeg -n $other $ins $maps $vopts -pass 2 $lopts $filters $aopts $sopts $metadata \"$outfull\""
+	else
+		command="ffmpeg -n $other $ins $maps $vopts $lopts $filters $aopts $sopts $metadata \"$outfull\""
+	fi
+	
 	# off we go!!
 	if $preview ; then
 		echo " "
