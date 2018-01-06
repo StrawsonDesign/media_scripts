27e966be97f558b18e977dfcac880fc4d993d96e
James Strawson <james@strawsondesign.com>
2018-01-05 23:48:51 -0800

    update dependencies in install scripts

--- install.sh
+++ install.sh
@@ -7,10 +7,13 @@ if [ "$EUID" -ne 0 ]
 	exit 1
 fi
 
-#install -m 755 media_scripts.sh /usr/local/bin/media_scripts
+install -m 755 media_scripts.sh /usr/local/bin/media_scripts
 
-sudo ln -s media_scripts.sh /usr/local/bin/media_scripts
-sudo apt install ffmpeg mkvtoolnix
+#chmod +x media_scripts.sh
+#sudo rm -f /usr/local/bin/media_scripts
+#sudo ln -s  media_scripts.sh /usr/local/bin/media_scripts 
+#sudo chmod +x /usr/local/bin/media_scripts
+sudo apt install ffmpeg mkvtoolnix liblept5 libtesseract-data libtesseract-dev libtesseract3
 sudo dpkg -i BDSup2Sub.deb vobsub2srt-1.0pre7-11-g0ba6e25-Linux.deb
 
 echo "DONE!"
